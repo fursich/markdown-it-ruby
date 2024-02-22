bf82835c594295dfe120f16e826ca3adbcef938d
Koji Onishi <fursich0@gmail.com>
2024-02-22 21:02:51 +0900

    bundle gem --ext=rust

---
+++ lib/markdown_rustified.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative "markdown_rustified/version"
+require_relative "markdown_rustified/markdown_rustified"
+
+module MarkdownRustified
+  class Error < StandardError; end
+  # Your code goes here...
+end
